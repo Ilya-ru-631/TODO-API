7d61c0a3917cca69f55d75662f5ed62189041323
sky <yuntcamokat@gmail.com>
2026-07-24 21:34:39 +0300

    V4. Update main. Added a router chi

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+
 	"todo_api/internal/handler"
 	"todo_api/internal/repository"
 )
@@ -11,13 +15,20 @@ func main() {
 	repo := repository.NewMemoryRepo()
 	h := handler.NewTaskHandler(repo)
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("GET /tasks", h.GetAll)
-	mux.HandleFunc("GET /tasks/{id}", h.GetByID)
-	mux.HandleFunc("POST /tasks", h.Create)
-	mux.HandleFunc("PUT /tasks/{id}", h.Update)
-	mux.HandleFunc("DELETE /tasks/{id}", h.Delete)
-	if err := http.ListenAndServe("localhost:8080", mux); err != nil {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Route("/tasks", func(r chi.Router) {
+		r.Get("/", h.GetAll)
+		r.Get("/{id}", h.GetByID)
+		r.Post("/", h.Create)
+		r.Put("/{id}", h.Update)
+		r.Delete("/{id}", h.Delete)
+	})
+
+	if err := http.ListenAndServe("localhost:8080", r); err != nil {
 		log.Fatal(err)
 	}
 }
